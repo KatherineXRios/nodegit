b6d3729997e840993c5091925d7c8885014ca342
Katherine Rios <katherinex.rios.lopez@gmail.com>
2020-07-06 15:48:49 -0400

    edit 2

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ var http = require('http');
 http.createServer(function (req, res) {
     console.log('texto cualquiera');
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Esta es la prueba final de git y node, si no RIP Katt');
+    res.end('Esta es la prueba final de git y node, si no QEPD Katt');
     
 }).listen(3000);
 
