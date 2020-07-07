2b834fd2f6b649e440a784fc528a33f527b1221f
Katherine Rios <katherinex.rios.lopez@gmail.com>
2020-07-06 23:29:00 -0400

    update index

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ var http = require('http');
 http.createServer(function (req, res) {
     console.log('texto cualquiera');
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Este texto lo cambio para probarlo, y si funciona bien si no, morirse');
+    res.end('Prueba ahora con dockers');
     
 }).listen(3000);
 
