743868ac992da826690aa33af02f76d890ac05a7
Katherine Rios <katherinex.rios.lopez@gmail.com>
2020-07-06 23:38:17 -0400

    update all

--- api/index.js
+++ api/index.js
@@ -1,9 +1,11 @@
 var http = require('http');
+const random = require('random');
 
 http.createServer(function (req, res) {
-    console.log('texto cualquiera');
+    console.log('texto cualquiera');    
+    var numero = random.int(0, 100);
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Prueba ahora con dockers');
+    res.end('Prueba ahora con dockers' + numero);
     
 }).listen(3000);
 
