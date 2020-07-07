3e542a131a37ff74c1790457148d76a9f50004c8
Katherine Rios <katherinex.rios.lopez@gmail.com>
2020-07-06 23:00:53 -0400

    update

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ var http = require('http');
 http.createServer(function (req, res) {
     console.log('texto cualquiera');
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Este texto lo cambio para probarlo, por favor funciona, :C');
+    res.end('Este texto lo cambio para probarlo, y si funciona bien si no, morir');
     
 }).listen(3000);
 
