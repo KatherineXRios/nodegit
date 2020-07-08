b95d1c2142ba2c6053ecb0144626b60f390e88a0
Katherine Rios <katherinex.rios.lopez@gmail.com>
2020-07-08 12:40:10 -0400

    Update all

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ http.createServer(function (req, res) {
     console.log('texto cualquiera');    
     var numero = random.int(0, 100);
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Prueba ahora con dockers' + numero);
+    res.end('Prueba ahora con dockers Kim y Katt' + numero);
     
 }).listen(3000);
 
